ec10b79e64a4f3a23e6e57df3d2c8b66ef8c6e20
vanty <lovelyzkim4@gmail.com>
2026-09-02 18:56:36 +0700

    Update OccupantTest.java

--- src/test/java/nl/tudelft/jpacman/board/OccupantTest.java
+++ src/test/java/nl/tudelft/jpacman/board/OccupantTest.java
@@ -31,8 +31,7 @@ class OccupantTest {
      */
     @Test
     void noStartSquare() {
-        // Remove the following placeholder:
-        assertThat(unit).isNotNull();
+        assertThat(unit.hasSquare()).isFalse();
     }
 
     /**
@@ -41,8 +40,9 @@ class OccupantTest {
      */
     @Test
     void testOccupy() {
-        // Remove the following placeholder:
-        assertThat(unit).isNotNull();
+        Square square = new BasicSquare();
+        unit.occupy(square);
+        assertThat(unit.getSquare()).isEqualTo(square);
     }
 
     /**
@@ -51,7 +51,14 @@ class OccupantTest {
      */
     @Test
     void testReoccupy() {
-        // Remove the following placeholder:
-        assertThat(unit).isNotNull();
+        Square firstSquare = new BasicSquare();
+        Square secondSquare = new BasicSquare();
+
+        unit.occupy(firstSquare);
+        unit.occupy(secondSquare);
+
+        assertThat(unit.getSquare()).isEqualTo(secondSquare);
+        assertThat(firstSquare.getOccupants()).doesNotContain(unit);
+        assertThat(secondSquare.getOccupants()).contains(unit);
     }
 }
